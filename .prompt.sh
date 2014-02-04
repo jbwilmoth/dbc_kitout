f66ae9e9124acbf07112dc56098746b5c16e3cf0
BATMAN <apprentice@dbc-chicago-34.local>
2014-02-03 18:28:08 -0600

    more config files

---
+++ .prompt.sh
@@ -0,0 +1,25 @@
+source /usr/local/etc/bash_completion.d/git-prompt.sh
+source ~/.bash-colors.sh
+
+RUBY=$'\xf0\x9f\x94\xbb'
+ARROW=$'\xe2\x9e\x9c'
+
+scm_ps1() {
+    local s=
+    if [[ -d ".svn" ]] ; then
+        s=\ \(svn:$(svn info | sed -n -e '/^Revision: \([0-9]*\).*$/s//\1/p' )\)
+    else
+        GIT_PS1_SHOWDIRTYSTATE=true
+        s=$(__git_ps1 " (git:%s)")
+    fi
+    echo -n "$s"
+}
+
+rbenv_ruby_version() {
+  if which rbenv > /dev/null; then
+    rbenv version | sed 's/\([^ ]*\).*/\1/'
+  fi
+}
+
+export GIT_PS1_SHOWDIRTYSTATE=true
+export PS1="$Yellow\u@\h$ResetColor_Off:$Cyan\w$Green\$(__git_ps1) $IRed$RUBY $(rbenv_ruby_version)$ResetColor_Off\n$White$ARROW  $ResetColor_Off"
